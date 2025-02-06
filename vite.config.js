04ece147815cdbb8d5e8923d19c3e5ee5305f717
mattp532 <youremail@example.com>
2025-02-06 15:43:01 -0500

    newthing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/valentines-day/', // Replace 'yourrepository' with the name of your repository
+  base: '/valentines-day6/', // Replace 'yourrepository' with the name of your repository
   build: {
     outDir: 'dist', // Default output directory for Vite builds
   },
